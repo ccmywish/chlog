8d2b17b1f1ee4215a3290c53845e317970f11f8e
Aoran Zeng <ccmywish@qq.com>
2023-05-09 21:23:01 +0800

    Move version out

---
+++ lib/chlog/version.rb
@@ -0,0 +1,16 @@
+# ---------------------------------------------------------------
+# File          : version.rb
+# Authors       : Aoran Zeng <ccmywish@qq.com>
+# Created on    : <2023-05-09>
+# Last modified : <2023-05-09>
+#
+# version:
+#
+#   Lib version
+# ---------------------------------------------------------------
+
+module Chlog
+
+  GEM_VERSION = "0.8.1"
+
+end
